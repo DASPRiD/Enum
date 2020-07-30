588a9c76da37b1365288938066289c70beaeeca3
William Desportes <williamdes@wdes.fr>
2020-07-30 13:50:20 +0200

    Fix setUp method signature for phpunit

--- test/AbstractEnumTest.php
+++ test/AbstractEnumTest.php
@@ -14,7 +14,7 @@ use ReflectionClass;
 
 final class AbstractEnumTest extends TestCase
 {
-    public function setUp()
+    public function setUp(): void
     {
         $reflectionClass = new ReflectionClass(AbstractEnum::class);
 
